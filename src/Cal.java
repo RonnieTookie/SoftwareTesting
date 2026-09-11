1a275445fc6b620dc3526cfef699b862bec6c714
RonnieTookie <Erronburton2021@gmail.com>
2026-09-10 21:20:02 -0400

    all test cases added and bugs fixed

--- src/Cal.java
+++ src/Cal.java
@@ -2,8 +2,7 @@
 public class Cal
 {
    public static int cal (int month1, int day1, int month2,
-                          int day2, int year)
-   {
+                          int day2, int year) {
    //***********************************************************
    // Calculate the number of days between the two given days in
    // the same year in accordance with the Gregorian calendar.
@@ -28,27 +27,28 @@ public class Cal
       int numDays;
  
       if (month2 == month1) // in the same month
-         numDays = day2 % day1;
+         numDays = day2 - day1; //modulo does not find the difference between numbers
       else {
          // The first array element is just a placeholder
          // because there is no month zero
-         int daysIn[] = {0, 31, 0, 31, 30, 31, 30, 31, 30, 30, 31, 30, 31};
+         int daysIn[] = {0, 31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
          // Are we in a leap year?
-         int m4 = year % 4;
-         int m100 = year % 100;
-         int m400 = year / 400;
-         if ((m4 != 0) || ((m100 == 0) || (m400 != 0)))
-            daysIn[2] = 28;
-         else
-            daysIn[2] = 29;
- 
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+          daysIn[2] = 29; 
+        }
+        else {
+         daysIn[2] = 28; 
+      }
          // start with days in the two months
          numDays = day2 + (daysIn[month1] - day1);
  
          // add the days in the intervening months
-         for (int i = month1 + 1; i <= month2; i++)
+         for (int i = month1 + 1; i < month2; i++)
             numDays = daysIn[i] + numDays;
       }
       return (numDays);
+   
+   }
+   public static void main(String[] args) {
    }
 }
--- src/calTest.java
+++ src/calTest.java
@@ -3,7 +3,61 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class calTest{
    @Test
-void testJanuaryFirstToJanuarySecond() {
+   void testJanuaryFirstToJanuarySecond() {
     assertEquals(1, Cal.cal(1, 1, 1, 2, 2024));
 }
+   @Test
+   void testDifMonth() {
+    assertEquals(122, Cal.cal(1,1,5,3,2023));
+   }
+
+    @Test
+    void testSameDay() {
+        assertEquals(0, Cal.cal(1, 1, 1, 1, 2024));
+    }
+
+    @Test
+    void testJanuaryFirstToJanuaryThirtyFirst() {
+        assertEquals(30, Cal.cal(1, 1, 1, 31, 2024));
+    }
+
+    @Test
+    void testLeapYear() {
+        assertEquals(60, Cal.cal(1, 1, 3, 1, 2024));
+    }
+    /** 
+   @Test
+   void invalidYear() {
+    boolean isValid = Cal.cal(2,4,5,6,30000);
+    assertFalse(isValid, "Invalid year");
+   }
+
+   @Test
+   void largerMonth1() {
+    assertFalse(Cal.cal(6,4,4,4,2002));
+   }
+   @Test
+   void DayBiggerThanMonth(){
+    assertfalse(Cal.cal(9,3,10,39,1973));
+   }
+}
+ // Calculate the number of days between the two given days in
+   // the same year in accordance with the Gregorian calendar.
+   // Examples: January 1 to January 1 is 0 days
+   //           January 1 to January 2 is 1 day
+   //           January 1 to January 31 is 30 days
+   // Preconditions:
+   //   1. day1 and day2 must be in same year
+   //   2. 1 <= month1, month2 <= 12
+   //   3. 1 <= day1 <= maxDaysInMonth1
+   //      where maxDaysInMonth1 is defined as the largest valid
+   //      day in the specified month1 and year, including any
+   //      leap year, as per the Gregorian calendar
+   //   4. 1 <= day2 <= maxDaysInMonth2
+   //      where maxDaysInMonth2 is defined as the largest valid
+   //      day in the specified month2 and year, including any
+   //      leap year, as per the Gregorian calendar
+   //   5. month1 <= month2
+   //   6. month1 == month2 implies day1 <= day2
+   //   7. 1 <= year <= 10000 **/
 }
